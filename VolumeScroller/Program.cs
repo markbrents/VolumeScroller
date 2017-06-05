19698c75bba53fa1c58fd6e38fab231033b0f40c
Mark Brents <markbrents@hotmail.com>
2017-06-04 19:52:34 -0500

    Added mutex so that only one version at a time will run. Also so the installer/uninstaller can detect if the program is running and ask the user to exit it before continuing.

--- VolumeScroller/Program.cs
+++ VolumeScroller/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace VolumeScroller
 {
     static class Program
     {
+        private static Mutex mutx;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bool createdNew;
+
+            mutx = new Mutex(true, "VolumeScrollerMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("Volume Scroller is already running");
+                return; 
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
